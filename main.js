6e4bc899cb5145cf56fa995363061aac141cee7e
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-06-09 22:02:24 -0400

    app running now

--- main.js
+++ main.js
@@ -6,13 +6,20 @@ function createWindow () {
       height: 600
     })
 
-
     win.loadFile('index.html')
-  }
+}
 
-  
-  app.whenReady().then(() => {
-    createWindow()
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
+})
+
 
   
