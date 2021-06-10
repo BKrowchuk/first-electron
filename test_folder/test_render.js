87a2e3ebdb36c5708ff1e61e28b0d9de600f5e2c
Benjamin James Carey Krowchuk <krowchuk16@gmail.com>
2021-06-09 23:07:18 -0400

    screen recording

---
+++ test_folder/test_render.js
@@ -0,0 +1,47 @@
+import { desktopCapturer, remote } from 'electron';
+const {Menu} = remote;
+
+// Buttons 
+const videoElement = document.getElementById("video");
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
+videoSelectBtn.onclick= getVideoSources;
+
+
+// Get the available video sources
+async function getVideoSources() {
+    const inputSources = await desktopCapturer.getSources({
+        types: ['window', 'screen']
+    });
+
+    const videoOptionsMenus = Menu.buildFromTemplate(
+        inputSources.map(source => {
+            return{
+                label: source.name,
+                click: () => selectSource(source)
+            };
+        })
+    );
+    videoOptionsMenus.popup();
+}
+
+async function selectSource(source) {
+    videoSelectBtn.innerText = source.name;
+
+    const constraints = {
+        audio: false,
+        video: {
+            mandatory: {
+                chromeMediaSource: 'desktop',
+                chromeMediaSourceId: source.id
+            }
+        }
+    };
+
+    const stream = await navigator.mediaDevices
+        .getUserMedia(constraints);
+    
+    videoElement.srcObject = stream;
+    videoElement.onplay();
+}
